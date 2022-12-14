6ff705bfc0e38347c060630587421172a04b1155
Luan Munholi <luanmunholi@gmail.com>
2022-12-14 02:50:15 -0300

    .

---
+++ Assets/HunterAI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HunterAI : MonoBehaviour {
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Route
+    public Vector3 walkPoint;
+    //States
+    public float sightRange;
+    public bool playerInSightRange;
+
+    private void Awake() {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (playerInSightRange) {
+            ChasePlayer();
+        }
+    }
+    
+    private void ChasePlayer() {
+        agent.SetDestination(player.position);
+    }
+}
